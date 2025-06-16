53fe7ffed9ca0fafd8b4700f597085e115cd3488
Sergei Shtepa <sergei.shtepa@veeam.com>
2025-06-16 11:52:48 +0200

    change the message tag from Error to Warning

--- pkg/deb/post_install.sh
+++ pkg/deb/post_install.sh
@@ -1,6 +1,6 @@
 if [ -z "$(dkms status -m blksnap -v #PACKAGE_VERSION# -k $(uname -r) | grep 'installed')" ]
 then
-	echo "ERROR: [TBD]The 'linux-headers' package for the current '$(uname -r)' kernel was not found."
-	echo "ERROR: [TBD]Install the 'linux-headers-$(uname -r)' package into the system."
-	echo "ERROR: [TBD]Or Install latest 'linux-image-$(dpkg --print-architecture)' and 'linux-headers-$(dpkg --print-architecture)' packages and reboot the system."
+	echo "WARNING: [TBD]The 'linux-headers' package for the current '$(uname -r)' kernel was not found."
+	echo "WARNING: [TBD]Install the 'linux-headers-$(uname -r)' package into the system."
+	echo "WARNING: [TBD]Or Install latest 'linux-image-$(dpkg --print-architecture)' and 'linux-headers-$(dpkg --print-architecture)' packages and reboot the system."
 fi
--- pkg/rpm/blksnap-kmp-suse/blksnap-post.sh
+++ pkg/rpm/blksnap-kmp-suse/blksnap-post.sh
@@ -12,12 +12,12 @@ then
 				then
 					echo "[TBD]The Veeam Software certificate is installed."
 				else
-					echo "ERROR: [TBD]The Veeam Software certificate should be installed."
-					echo "ERROR: [TBD]Please install 'veeam-ueficert' package and complete MOK enrollment to continue."
+					echo "WARNING: [TBD]The Veeam Software certificate should be installed."
+					echo "WARNING: [TBD]Please install 'veeam-ueficert' package and complete MOK enrollment to continue."
 				fi
 			else
-				echo "ERROR: [TBD]The 'mokutil' is not installed."
-				echo "ERROR: [TBD]Please install 'mokutil' package, install 'veeam-ueficert' package and complete MOK enrollment to continue."
+				echo "WARNING: [TBD]The 'mokutil' is not installed."
+				echo "WARNING: [TBD]Please install 'mokutil' package, install 'veeam-ueficert' package and complete MOK enrollment to continue."
 			fi
 		fi
 	fi
